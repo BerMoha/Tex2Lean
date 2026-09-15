b8879538a4960fa0ae73d4c720a37ee6b06a0e48
BerMoha <berkanimoha@gmail.com>
2026-09-15 11:41:19 +0100

    Fix Mathlib dependency: use v4.33.0 tag for cache availability

--- authoring/lakefile.lean
+++ authoring/lakefile.lean
@@ -9,4 +9,4 @@ lean_lib Tex2lean where
   srcDir := "."
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "db584cd6d46c92f209a44c0f1c829460d327499d"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.33.0"
